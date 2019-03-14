b72f29b6cad1da4759d27429b9e11aed71f3cc78
Arnon <arnon.b@zooz.com>
2019-03-14 12:07:28 +0200

    format code

--- server.js
+++ server.js
@@ -1,39 +1,37 @@
-const express = require('express')
-const axios = require('axios')
-const cors = require('cors')
-var app = express();
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const path = require('path');
 
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/src/js'));
-app.use(express.static(__dirname + '/src/css'));
-app.use(cors())
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src', 'js')));
+app.use(express.static(path.join(__dirname, 'src', 'css')));
+app.use(cors());
 
 
-app.get('/supportedpaymentmethods', function (req, res) {
-    var headers = {
-            // Replace app-id and private-key with the id and private key of your Business Unit.
-            // The default provider configured in this Business Unit must support the Get Supported Payment Methods API.
-            'app-id': 'com.xxx.xxx',
-            'private-key': 'xxxxxxxxxxxxx',
-            'x-payments-os-env': 'test',
-            'api-version': '1.2.0'
-    };
-    
-    axios.get("https://api.paymentsos.com/supported-payment-methods", {
-        'headers': headers}).then((response)=>{
+app.get('/supportedpaymentmethods', (req, res) => {
+  const headers = {
+    // Replace app-id and private-key with the id and private key of your Business Unit.
+    // The default provider configured in this Business Unit must support the Get Supported Payment Methods API.
+    'app-id': 'com.xxx.xxx',
+    'private-key': 'xxxxxxxxxxxxx',
+    'x-payments-os-env': 'test',
+    'api-version': '1.2.0',
+  };
 
-         res.send(response.data)
-
-    
-    }).catch((e)=>{
-            console.log(e.response.data)
-            res.send(e.response.data)
-       
+  axios.get('https://api.paymentsos.com/supported-payment-methods', { headers })
+    .then((response) => {
+      res.send(response.data);
     })
+    .catch((e) => {
+      console.log(e.response.data);
+      res.send(e.response.data);
+    });
+});
 
-  });
 
-
-app.listen('9000', ()=>{
-    console.log(`server is up on port 9000`)
-}); 
+app.listen('9000', () => {
+  console.log('server is up on port 9000');
+});
